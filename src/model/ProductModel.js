247e2fd92f1cb0308e641c3f1ae761207a2ba459
TVQ <tavanquyet2206@gmail.com>
2023-02-05 21:20:44 +0700

    sua api

--- src/model/ProductModel.js
+++ src/model/ProductModel.js
@@ -6,6 +6,7 @@ const productSchema = new mongoose.Schema(
         image: { type: String, required: true },
         price: { type: Number, required: true },
         countInStock: { type: Number, required: true },
+        count: { type: Number, required: true, default: 1, },
         rating: { type: Number, required: true },
         description: { type: String, required: true },
         favourite: { type: Boolean, required: true },
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -2,32 +2,38 @@ const Product = require('../model/ProductModel');
 
 const createProduct = (newProduct) => {
     return new Promise(async (resolve, reject) => {
-        const { name, image, countInStock, price, favourite, rating, description } = newProduct
+        const { name, image, countInStock, price, favourite, rating, description } = newProduct;
         try {
             const checkProduct = await Product.findOne({
-                name: name
-            })
+                name: name,
+            });
             if (checkProduct !== null) {
                 resolve({
                     status: 'ERR',
-                    message: 'The name of product is already'
-                })
+                    message: 'The name of product is already',
+                });
             }
             const newProduct = await Product.create({
-                name, image, countInStock, price, favourite, rating, description
-            })
+                name,
+                image,
+                countInStock,
+                price,
+                favourite,
+                rating,
+                description,
+            });
             if (newProduct) {
                 resolve({
                     status: 'OK',
                     message: 'SUCCESS',
-                    data: newProduct
-                })
+                    data: newProduct,
+                });
             }
         } catch (e) {
-            reject(e)
+            reject(e);
         }
-    })
-}
+    });
+};
 
 module.exports = {
     createProduct,
